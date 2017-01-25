7fbf7846f80c58539eded616d6b545e929ab1a8b
Josh Long <josh@joshlong.com>
2017-01-25 14:38:02 -0800

    adding the Spring Data Couchbase Spring Tip code.

--- reactive-client/src/main/java/com/example/ReactiveClientApplication.java
+++ reactive-client/src/main/java/com/example/ReactiveClientApplication.java
@@ -11,8 +11,54 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.springframework.web.reactive.function.client.ClientRequest.GET;
+
+
+class MatchMaker {
+
+	public MatchMaker() {
+		Flux<Person> men = this.men();
+		Flux<Person> women = this.women();
+		Flux<Tuple2<Person, Person>> zip = Flux.zip(women, men).delay(Duration.of(1, ChronoUnit.SECONDS));
+		zip.subscribe(tpl -> System.out.println(tpl.getT1().getName() + " and " + tpl.getT2().getName() + " sitting in a tree.."));
+	}
+
+	private Person randomPerson(String names[]) {
+		int nameIndx = new Random().nextInt(names.length);
+		String name = names[nameIndx];
+		int age = new Random().nextInt(100);
+		return new Person(name, age);
+	}
+
+	private Person randomWomen() {
+		String[] names = {"Jane", "Janet", "Joan", "Linda", "Laura", "Marie", "Maria", "Penelope", "Pippi", "Chunhua"};
+		return randomPerson(names);
+	}
+
+	private Person randomMen() {
+		String[] names = {"Bob", "Joe", "Ling", "Jaime", "Jesus", "Georg", "Danny", "Hans"};
+		return randomPerson(names);
+	}
+
+	private Flux<Person> women() {
+		return Flux.fromStream(Stream.generate(this::randomWomen));
+	}
+
+	private Flux<Person> men() {
+		return Flux.fromStream(Stream.generate(this::randomMen));
+	}
+
+
+}
+
 
 
 @SpringBootApplication
@@ -20,12 +66,12 @@ public class ReactiveClientApplication {
 
 	private Log log = LogFactory.getLog(getClass());
 
-	@Bean
+
+	//@Bean
 	CommandLineRunner demo(WebClient webClient) {
 		return args -> {
 
 			// dependent calls
-			// TODO: my kingdom for a logger
 			webClient.exchange(GET("http://localhost:8080/persons/588409d868dde8065d47edfa").build())
 					.flatMap(clientResponse -> clientResponse.bodyToMono(Person.class))
 					.subscribe(log::info);
@@ -59,4 +105,9 @@ class Person {
 	private String id;
 	private String name;
 	private int age;
+
+	Person(String name, int a) {
+		this.age = a;
+		this.name = name;
+	}
 }
