967a6995283f2b83139d860201a8d3d1f71db965
Josh Long <josh@joshlong.com>
2017-09-02 13:18:42 +0800

    jaxrs

--- jaxrs/src/main/java/com/example/jaxrs/JaxrsApplication.java
+++ jaxrs/src/main/java/com/example/jaxrs/JaxrsApplication.java
@@ -1,55 +1,20 @@
 package com.example.jaxrs;
 
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.ApplicationPath;
+import java.util.stream.Stream;
 
 @SpringBootApplication
+@ApplicationPath("/root")
 public class JaxrsApplication {
-
+    JaxrsApplication(CustomerRepository customerRepository) {
+        Stream.of("A", "B", "C").forEach(c -> customerRepository.save(new Customer(null, c)));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JaxrsApplication.class, args);
     }
 }
 
-interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
-
-@Entity
-@Data
-class Customer {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String name;
-}
-
-@Component
-@Path("/customers")
-class CustomerResource {
-
-    private final CustomerRepository customerRepository;
-
-    CustomerResource(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    @GET
-    @Path("/{id}")
-    public Response byId(@PathParam("id") Long id) {
-        return Response.ok(this.customerRepository.findById(id)).build();
-    }
-}
