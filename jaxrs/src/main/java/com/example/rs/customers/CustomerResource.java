beb1ebef67f543378826f5981bd03bb2ea21d87c
Josh Long <josh@joshlong.com>
2017-09-03 03:38:05 +0800

    JAX-RS configuration redux.

--- jaxrs/src/main/java/com/example/rs/customers/CustomerResource.java
+++ jaxrs/src/main/java/com/example/rs/customers/CustomerResource.java
@@ -7,7 +7,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.Optional;
 
 /**
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
