c30abdbd10f810a2fe8c5b10ec6e18169d8d7de3
Josh Long <josh@joshlong.com>
2017-09-03 03:38:26 +0800

    cleanup

--- jaxrs/src/main/java/com/example/rs/JerseyApplication.java
+++ jaxrs/src/main/java/com/example/rs/JerseyApplication.java
@@ -2,7 +2,6 @@ package com.example.rs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JerseyApplication {
--- jaxrs/src/main/java/com/example/rs/customers/CustomerNotFoundException.java
+++ jaxrs/src/main/java/com/example/rs/customers/CustomerNotFoundException.java
@@ -1,7 +1,6 @@
 package com.example.rs.customers;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
